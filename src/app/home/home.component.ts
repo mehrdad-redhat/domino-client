770b5acb7167df80eaf8ff253be8b7f495ad3a1b
Mehrdad Helmi <mehrdad.redhat@gmail.com>
2018-12-04 09:10:16 +0330

    + make curve perfected + about section + features section + blog section + gooey effect!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 
 @Component({
   selector: 'd-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
 
   ngOnInit() {
+  }
+  @ViewChild('selectedNum') selectedNum;
+  @ViewChildren('numbers') gooNumbers:QueryList<ElementRef>;
+  ngAfterViewInit(){
+    console.log(this.selectedNum);
+    console.log(this.gooNumbers.toArray()[0]);
+    this.selectedNum.nativeElement.style.left=this.gooNumbers.toArray()[0].nativeElement.offsetLeft-8+'px';
+  }
+
+  moveGoo(event){
+    let target = event.target || event.srcElement || event.currentTarget;
+    console.log(target);
+    this.selectedNum.nativeElement.style.left=target.offsetLeft-8+'px';
   }
 
 }
