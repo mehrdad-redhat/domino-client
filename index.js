0e0a2b95406e2042d7efb77133b922e58f185fac
Mehrdad Helmi <mehrdad.redhat@gmail.com>
2018-12-08 19:36:48 +0330

    + http server

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-// connect().use(serveStatic(__dirname + '/dist')).listen(8080, function(){
-//   console.log('Server running on 8080...');
-// });
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+];
+
+app.get('*', (req , res) => {
+  if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+    res.sendFile(path.resolve(`dist/${req.url}`));
+  } else {
+    res.sendFile(path.resolve('dist/index.html'));
+  }
+});
+
+
+app.listen(8080);
